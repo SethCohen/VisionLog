ec1626023cd96e25e08e2f6987bc3f59f7830139
SethCohen <47002293+SethCohen@users.noreply.github.com>
2020-09-15 15:15:18 -0400

    Test commit

--- app/src/main/java/seth/cohen/visionlog/MainActivity.java
+++ app/src/main/java/seth/cohen/visionlog/MainActivity.java
@@ -13,7 +13,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-    //test to see if git works
+    //test to see if git work
     private Toast backPressedToast;
     long back_pressed;
 
