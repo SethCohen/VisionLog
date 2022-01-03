1e3301eaa71b27bebf0fa07ed070ae8914e8b6ce
SethCohen <sethcohen.dev@gmail.com>
2022-01-03 00:42:44 -0500

    Added basic pie chart for dream feels

--- visionlog/lib/pages/statistics_page.dart
+++ visionlog/lib/pages/statistics_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:visionlog/widgets/feel_pie_chart.dart';
 
 class StatisticsPage extends StatefulWidget {
   StatisticsPage({Key? key}) : super(key: key);
@@ -8,7 +9,6 @@ class StatisticsPage extends StatefulWidget {
 }
 
 class _StatisticsPageState extends State<StatisticsPage> {
-
   @override
   void initState() {
     super.initState();
@@ -19,14 +19,48 @@ class _StatisticsPageState extends State<StatisticsPage> {
     return Scaffold(
         appBar: AppBar(
           title: Text('Statistics'),
+          actions: [
+            PopupMenuButton(
+              icon: Icon(Icons.date_range),
+              itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+                const PopupMenuItem(
+                  value: 0,
+                  child: Text('All-Time'),
+                ),
+                const PopupMenuItem(
+                  value: 1,
+                  child: Text('Yearly'),
+                ),
+                const PopupMenuItem(
+                  value: 2,
+                  child: Text('Semi Yearly'),
+                ),
+                const PopupMenuItem(
+                  value: 3,
+                  child: Text('Quarterly'),
+                ),
+                const PopupMenuItem(
+                  value: 4,
+                  child: Text('Monthly'),
+                ),
+                const PopupMenuItem(
+                  value: 5,
+                  child: Text('Weekly'),
+                ),
+              ],
+            ),
+          ],
         ),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: ListView(
-            children: [],
+          child: Column(
+            children: [
+              SizedBox(
+                height: 300.0,
+                child: FeelPieChart(),
+              )
+            ],
           ),
         ));
   }
-
-
 }
---
+++ visionlog/lib/widgets/feel_pie_chart.dart
@@ -0,0 +1,120 @@
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/src/provider.dart';
+import 'package:visionlog/provider/dream_documents_provider.dart';
+import 'package:visionlog/widgets/dream.dart';
+
+class FeelPieChart extends StatelessWidget {
+
+  FeelPieChart();
+
+  @override
+  Widget build(BuildContext context) {
+    var docs = context.watch<Documents>().documents;
+
+    List<charts.Series<MoodCount, String>> _series = [
+      charts.Series<MoodCount, String>(
+        id: 'Mood',
+        domainFn: (MoodCount mood, _) => mood.feel,
+        measureFn: (MoodCount mood, _) => mood.count,
+        colorFn: (MoodCount mood, _) => mood.color,
+        data: _buildData(docs),
+        labelAccessorFn: (MoodCount row, _) =>
+            '${(row.count/docs!.length * 100).toStringAsFixed(1)}%',
+      ),
+    ];
+
+    return new charts.PieChart<String>(_series,
+        animate: true,
+        behaviors: [
+          new charts.DatumLegend(
+            position: charts.BehaviorPosition.bottom,
+            horizontalFirst: true,
+            desiredMaxRows: 2,
+            outsideJustification: charts.OutsideJustification.middleDrawArea,
+            cellPadding: new EdgeInsets.only(right: 8.0, bottom: 4.0),
+          )
+        ],
+        defaultRenderer: new charts.ArcRendererConfig(arcRendererDecorators: [
+          new charts.ArcLabelDecorator(
+            outsideLabelStyleSpec: charts.TextStyleSpec(
+                color: charts.MaterialPalette.white, fontSize: 15),
+            insideLabelStyleSpec: charts.TextStyleSpec(
+                color: charts.MaterialPalette.white, fontSize: 15),
+          ),
+        ]));
+  }
+
+  _buildData(List<QueryDocumentSnapshot<Object?>>? docs) {
+    double terribleCount = 0,
+        badCount = 0,
+        averageCount = 0,
+        okayCount = 0,
+        fantasticCount = 0;
+
+    if (docs != null) {
+      docs.forEach((DocumentSnapshot document) {
+        final dream = Dream.fromMap(document.data() as Map<String, dynamic>,
+            reference: document.reference);
+        switch (dream.feel) {
+          case 'terrible':
+            {
+              terribleCount++;
+            }
+            break;
+          case 'bad':
+            {
+              badCount++;
+            }
+            break;
+          case 'average':
+            {
+              averageCount++;
+            }
+            break;
+          case 'okay':
+            {
+              okayCount++;
+            }
+            break;
+          case 'fantastic':
+            {
+              fantasticCount++;
+            }
+            break;
+          default:
+            {
+              averageCount++;
+            }
+            break;
+        }
+      });
+
+      return [
+        MoodCount("terrible", terribleCount,
+            charts.ColorUtil.fromDartColor(Color(0xFF882255))),
+        MoodCount("bad", badCount,
+            charts.ColorUtil.fromDartColor(Color(0xFFDDCC77))),
+        MoodCount("average", averageCount,
+            charts.ColorUtil.fromDartColor(Color(0xFF88CCEE))),
+        MoodCount("okay", okayCount,
+            charts.ColorUtil.fromDartColor(Color(0xFF117733))),
+        MoodCount("fantastic", fantasticCount,
+            charts.ColorUtil.fromDartColor(Color(0xFF3A27A8))),
+      ];
+    }
+  }
+}
+
+class MoodCount {
+  final String feel;
+  final double count;
+  final charts.Color color;
+
+  MoodCount(this.feel, this.count, this.color);
+
+  String toString() {
+    return '\nMoodCount{feel: $feel, count: $count}';
+  }
+}
