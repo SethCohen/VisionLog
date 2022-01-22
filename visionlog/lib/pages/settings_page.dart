9880e946865a8b7c3102052d80cd7e316717c82a
SethCohen <sethcohen.dev@gmail.com>
2022-01-22 01:22:02 -0500

    Entire refactor to settings page. Added some new settings.

--- visionlog/lib/pages/settings_page.dart
+++ visionlog/lib/pages/settings_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:visionlog/widgets/sign_out.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SettingsPage extends StatefulWidget {
   SettingsPage({Key? key}) : super(key: key);
@@ -12,6 +13,26 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   final user = FirebaseAuth.instance.currentUser!;
+  PackageInfo _packageInfo = PackageInfo(
+    appName: 'Unknown',
+    packageName: 'Unknown',
+    version: 'Unknown',
+    buildNumber: 'Unknown',
+    buildSignature: 'Unknown',
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,46 +42,47 @@ class _SettingsPageState extends State<SettingsPage> {
         actions: [SignOut()],
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        padding: const EdgeInsets.fromLTRB(48.0, 8.0, 8.0, 8.0),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Text('General',
+                  textScaleFactor: 1.05,
+                  style: TextStyle(
+                      color: Colors.deepPurple, fontWeight: FontWeight.bold)),
               TextButton(
-                  onPressed: _launchGithubURL, // TODO finish export database
+                  onPressed: null, // TODO finish export database
                   child: Text(
                     'Export database',
                     style: TextStyle(color: Colors.white70),
                   )),
               TextButton(
-                  onPressed: _launchGithubURL,
-                  // TODO finish export old dreams
+                  onPressed: null, // TODO finish theme editor
                   child: Text(
-                    'Export old dreams',
-                    style: TextStyle(color: Colors.white70),
-                  )),
-              Divider(
-                color: Colors.white70,
-              ),
-              TextButton(
-                  onPressed: _launchReportIssueGithubURL,
-                  child: Text(
-                    'Report an issue via Github',
+                    'Appearance',
                     style: TextStyle(color: Colors.white70),
                   )),
               TextButton(
-                  onPressed: _launchReportIssueGmailURL,
+                  onPressed: null, // TODO finish account manager
                   child: Text(
-                    'Report an issue via Gmail',
+                    'Manage Account',
                     style: TextStyle(color: Colors.white70),
                   )),
-              Divider(
-                color: Colors.white70,
-              ),
+              Text('About',
+                  textScaleFactor: 1.05,
+                  style: TextStyle(
+                      color: Colors.deepPurple, fontWeight: FontWeight.bold)),
+              TextButton(
+                  onPressed: _launchPrivacyPolicyURL,  // TODO change privacy policy url to personal website
+                  child: Text(
+                    'Privacy Policy',
+                    style: TextStyle(color: Colors.white70),
+                  )),
               TextButton(
                   onPressed: _launchGithubURL,
                   child: Text(
-                    'Github',
+                    'Source Code',
                     style: TextStyle(color: Colors.white70),
                   )),
               TextButton(
@@ -70,30 +92,126 @@ class _SettingsPageState extends State<SettingsPage> {
                     style: TextStyle(color: Colors.white70),
                   )),
               TextButton(
-                  onPressed: _launchPrivacyPolicyURL,
-                  child: Text(
-                    'Privacy Policy',
-                    style: TextStyle(color: Colors.white70),
-                  )),
-              TextButton(
                   onPressed: null, // TODO changelog
                   child: Text(
                     'Recent Changes',
                     style: TextStyle(color: Colors.white70),
                   )),
-              Divider(
-                color: Colors.white70,
-              ),
               TextButton(
-                  onPressed: _launchPersonalSiteURL,
+                  onPressed: null,
                   child: Text(
-                    'My Website',
+                    'Version: ${_packageInfo.version}',
+                    style: TextStyle(color: Colors.white70),
+                  )),
+              Text('Report Issues',
+                  textScaleFactor: 1.05,
+                  style: TextStyle(
+                      color: Colors.deepPurple, fontWeight: FontWeight.bold)),
+              TextButton(
+                  onPressed: _launchReportIssueGithubURL,
+                  child: Text(
+                    'Report An Issue Via Github',
                     style: TextStyle(color: Colors.white70),
                   )),
               TextButton(
-                  onPressed: _launchSupportURL, // TODO finish buy a coffee
+                  onPressed: _launchSendEmail,
                   child: Text(
-                    'Buy Me a Coffee',
+                    'Report An Issue Via Email',
+                    style: TextStyle(color: Colors.white70),
+                  )),
+              Text('Other',
+                  textScaleFactor: 1.05,
+                  style: TextStyle(
+                      color: Colors.deepPurple, fontWeight: FontWeight.bold)),
+              TextButton(
+                  onPressed: null, // TODO finish IAP
+                  child: Text(
+                    'Support Me',
+                    style: TextStyle(color: Colors.white70),
+                  )),
+              TextButton(
+                  onPressed: _launchPersonalSiteURL,
+                  child: Text(
+                    'Visit My Website',
+                    style: TextStyle(color: Colors.white70),
+                  )),
+              TextButton(
+                  onPressed: () => showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          backgroundColor: Colors.grey.shade900,
+                          title: const Text('Hi, I\'m Seth! 🍵',
+                              style: TextStyle(color: Colors.white)),
+                          content: SizedBox(
+                            height: 350,
+                            child: SingleChildScrollView(
+                              child: RichText(
+                                  textScaleFactor: 1.15,
+                                  text: TextSpan(
+                                      style: TextStyle(color: Colors.white),
+                                      text:
+                                          "I'm a freelancer and hobbyist who's passionate towards creating various apps, software, digital art, and graphic designs. I created this app for my own personal use to help me lucid dream better and just because I thought it'd be nice to record my dreams.\n\nAlong the way I thought it'd be nice to share my app with others, so here we are. I'll always try to focus on efficiency and simplicity so hopefully this app never becomes slow. Nor will there ever be ads.\n\nIf you'd like to see more of my work or contact me for work - as I am a freelancer - you can check out my personal website from the ",
+                                      children: [
+                                        TextSpan(
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold),
+                                            text: "Visit My Website"),
+                                        TextSpan(
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.normal),
+                                            text:
+                                                " button.\n\nOr if you'd like to show some support, you can "),
+                                        TextSpan(
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontStyle: FontStyle.italic),
+                                            text: "buy me a "),
+                                        TextSpan(
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                decoration:
+                                                    TextDecoration.lineThrough,
+                                                fontStyle: FontStyle.italic),
+                                            text: "coffee"),
+                                        TextSpan(
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontStyle: FontStyle.italic),
+                                            text: " tea"),
+                                        TextSpan(
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                            ),
+                                            text: " from the "),
+                                        TextSpan(
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold),
+                                            text: "Support Me"),
+                                        TextSpan(
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.normal),
+                                            text:
+                                                " button. I'd very much appreciate it! This app is completely free so I don't really make any money from it.\n\nAnyways, hope you enjoy this app and have great dreams!\nThank you for installing it. 🙂")
+                                      ])),
+                            ),
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () => Navigator.pop(context, 'OK'),
+                              child: const Text('OK',
+                                  style: TextStyle(color: Colors.white)),
+                              style: ButtonStyle(
+                                  splashFactory: NoSplash.splashFactory),
+                            ),
+                          ],
+                        ),
+                      ), //
+                  child: Text(
+                    'About Me',
                     style: TextStyle(color: Colors.white70),
                   )),
             ],
@@ -131,16 +249,6 @@ class _SettingsPageState extends State<SettingsPage> {
     }
   }
 
-  _launchSupportURL() async {
-    const url =
-        'https://play.google.com/store/apps/details?id=seth.cohen.visionlog&reviewId=0';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
   _launchReportIssueGithubURL() async {
     const url = 'https://github.com/SethCohen/VisionLog/issues';
     if (await canLaunch(url)) {
@@ -150,7 +258,7 @@ class _SettingsPageState extends State<SettingsPage> {
     }
   }
 
-  _launchReportIssueGmailURL() async {
+  _launchSendEmail() async {
     const url =
         'mailto:sethcohen.dev@gmail.com?subject=VisionLog Issue&body=Please answer each field as best as possible, blank answers are fine though. Thank you.\n______________\n\nPhone Model: \n\nPhone OS Version: \n\nDescription of issue: \n\nThe last thing you were doing before the issue happened: \n';
     if (await canLaunch(url)) {
